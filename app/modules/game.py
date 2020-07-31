e230a82766283253e52522f90e977281bd8b4896
Jonny Reading <jonnyreading@gmail.com>
2020-07-31 17:34:58 +0100

    play_round method returns the player who won

--- app/modules/game.py
+++ app/modules/game.py
@@ -4,4 +4,21 @@ class Game:
         self.valid_choices = ["rock", "paper", "scissors"]
 
     def play_round(self, player1, player2):
-        pass
+        if player1.choice == player2.choice:
+            return None
+        if player1.choice == "paper":
+            if player2.choice == "rock":
+                return player1
+            else:
+                return player2
+        if player1.choice == "rock":
+            if player2.choice == "paper":
+                return player2
+            else:
+                return player1
+        if player1.choice == "scissors":
+            if player2.choice == "paper":
+                return player1
+            else:
+                return player2
+
