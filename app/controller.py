b6648c8eacf470bba05ccb7301bdd25d48af0dbb
Jonny Reading <jonnyreading@gmail.com>
2020-07-31 17:08:16 +0100

    Filled in losing pages for player 1

--- app/controller.py
+++ app/controller.py
@@ -2,25 +2,37 @@ from app import app
 from flask import render_template, request, redirect
 
 @app.route("/")
-def hello_world():
+def home_page():
     return "Rock, Paper, Scissors game!"
 
 @app.route("/paper/rock")
 def paper_beats_rock():
     return "Player 1 wins playing paper against rock!"
 
+@app.route("/paper/scissors")
+def paper_loses_scissors():
+    return "Player 1 loses playing paper against scissors!"
+
 @app.route("/rock/scissors")
 def rock_beats_scissors():
     return "Player 1 wins playing rock against scissors!"
+
+@app.route("/rock/paper")
+def rock_loses_paper():
+    return "Player 1 loses playing rock against paper!"
 
 @app.route("/scissors/paper")
 def scissors_beats_paper():
     return "Player 1 wins playing scissors against paper!"
 
+@app.route("/scissors/rock")
+def scissors_loses_rock():
+    return "Player 1 loses playing scissors against rock!"
+
 @app.route("/paper/paper")
 @app.route("/rock/rock")
 @app.route("/scissors/scissors")
 def draw():
-    return "Draw! No one wins"
+    return "Draw! No one wins!"
 
 
