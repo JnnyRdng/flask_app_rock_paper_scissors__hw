6350e01a9b5929aa06636d50fe23a161b1dc2335
Jonny Reading <jonnyreading@gmail.com>
2020-07-31 12:12:30 +0100

    Paper beats rock

--- app/controller.py
+++ app/controller.py
@@ -3,4 +3,8 @@ from flask import render_template, request, redirect
 
 @app.route("/")
 def hello_world():
-    return "Hello, World!"
+    return "Rock, Paper, Scissors game!"
+
+@app.route("/paper/rock")
+def paper_beats_rock():
+    return "Player 1 wins playing paper against rock!"
