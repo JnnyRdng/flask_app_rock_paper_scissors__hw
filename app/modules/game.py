6a0a41fb933ce183bfacc37f28d073e7403c4be4
Jonny Reading <jonnyreading@gmail.com>
2020-07-31 17:51:53 +0100

    refactor play_round to check for valid input

--- app/modules/game.py
+++ app/modules/game.py
@@ -4,21 +4,26 @@ class Game:
         self.valid_choices = ["rock", "paper", "scissors"]
 
     def play_round(self, player1, player2):
-        if player1.choice == player2.choice:
-            return None
-        if player1.choice == "paper":
-            if player2.choice == "rock":
-                return player1
-            else:
-                return player2
-        if player1.choice == "rock":
-            if player2.choice == "paper":
-                return player2
-            else:
-                return player1
-        if player1.choice == "scissors":
-            if player2.choice == "paper":
-                return player1
-            else:
-                return player2
+        p1_choice = player1.choice.lower()
+        p2_choice = player2.choice.lower()
+        if p1_choice in self.valid_choices and p2_choice in self.valid_choices:
+            if p1_choice == p2_choice:
+                return None
+            if p1_choice == "paper":
+                if p2_choice == "rock":
+                    return player1
+                else:
+                    return player2
+            if p1_choice == "rock":
+                if p2_choice == "paper":
+                    return player2
+                else:
+                    return player1
+            if p1_choice == "scissors":
+                if p2_choice == "paper":
+                    return player1
+                else:
+                    return player2
+        else:
+            return "Not a valid input!"
 
