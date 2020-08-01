5e29b7c7e36d6c81f7cb60fe891d477fb3f3c3c3
Jonny Reading <jonnyreading@gmail.com>
2020-08-01 22:23:46 +0100

    Refactor templates, avoid repetition of code. Added a running score total

--- app/controller.py
+++ app/controller.py
@@ -8,13 +8,16 @@ from app.modules.logic import game, cpu, player, player_name
 def home_page():
     return render_template("welcome.html", title="Home", player_name=player_name)
 
+
 @app.route("/play")
 def play_game():
     return render_template("index.html", title="Play!")
 
+
 @app.route("/about")
 def about_page():
     return render_template("about.html", title="About")
+
 
 @app.route("/set-name", methods=["POST"])
 def get_player_name():
@@ -23,20 +26,14 @@ def get_player_name():
     player_name = new_name if new_name != "" else player_name
     return redirect("/play")
 
+
 @app.route("/<choice1>/<choice2>")
 def play_a_game(choice1, choice2):
     p1 = Player("Player 1", choice1)
     p2 = Player("Player 2", choice2)
     winner = game.play_round(p1, p2)
-    if winner == False:
-        return redirect("/")
-    elif winner is not None:
-        loser = p2 if p1 == winner else p1
-        return render_template(
-            "winner.html", title=f"{winner.name} Wins!", winner=winner, loser=loser
-        )
-    else:
-        return render_template("draw.html", title="It's a draw!", choice=p1.choice)
+    return route_winner(winner, p1, p2)
+
 
 @app.route("/get-choices", methods=["POST"])
 def get_choices():
@@ -46,17 +43,28 @@ def get_choices():
     player.choice = move
     game.decide_cpu_move(cpu)
     winner = game.play_round(player, cpu)
+    return route_winner(winner, player, cpu)
+
+
+def route_winner(winner, player, cpu):
+    sorted_scores = []
+    if player.score >= cpu.score:
+        sorted_scores.append(player)
+        sorted_scores.append(cpu)
+    else:
+        sorted_scores.append(cpu)
+        sorted_scores.append(player)
     if winner == False:
         return redirect("/")
     elif winner == player:
+        player.score += 1
         return render_template(
-            "winner.html", title=f"{player.name} Wins!", winner=player, loser=cpu
+            "winner.html", title=f"{player.name} Wins!", winner=player, loser=cpu, scores=sorted_scores
         )
     elif winner == cpu:
+        cpu.score += 1
         return render_template(
-            "loser.html", title=f"You lost!", winner=cpu, loser=player
+            "loser.html", title=f"You lost!", winner=cpu, loser=player, scores=sorted_scores
         )
     else:
-        return render_template("draw.html", title="It's a draw!", choice=player.choice)
-
-
+        return render_template("draw.html", title="It's a draw!", choice=player.choice, scores=sorted_scores)
