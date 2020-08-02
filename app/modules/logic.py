6df08381c89af52f70e670cb3f8566792ae8411b
Jonny Reading <jonnyreading@gmail.com>
2020-08-02 17:49:51 +0100

    added function to sort scores for scoreboard so highest score shown first

--- app/modules/logic.py
+++ app/modules/logic.py
@@ -7,3 +7,12 @@ player_name = "Player"
 player = Player(player_name, "rock")
 cpu = Player("Computer", "rock")
 
+def sort_scores():
+    sorted_scores = []
+    if player.score >= cpu.score:
+        sorted_scores.append(player)
+        sorted_scores.append(cpu)
+    else:
+        sorted_scores.append(cpu)
+        sorted_scores.append(player)
+    return sorted_scores
